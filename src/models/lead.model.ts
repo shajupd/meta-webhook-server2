328fc4f1d916d308845e51b6487e077a075fdf7a
Shaju PD <shajupd@webdura.tech>
2023-06-20 12:48:25 +0530

    Fix

--- src/models/lead.model.ts
+++ src/models/lead.model.ts
@@ -2,10 +2,14 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface LeadDocument extends Document {
   data: object;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const leadSchema = new Schema<LeadDocument>({
   data: { type: Schema.Types.Mixed, required: true },
+}, {
+  timestamps: true, // Enable timestamps
 });
 
 const Lead = mongoose.model<LeadDocument>('Lead', leadSchema);
